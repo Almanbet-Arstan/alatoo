e4d0ee17511d373fb346483532d8b72dad092449
Almanbet Totoev <almanbet.totoev@mail.ru>
2022-05-12 01:55:43 +0600

    first commit

---
+++ src/test/java/com/example/pizzaCafe/PizzaCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
